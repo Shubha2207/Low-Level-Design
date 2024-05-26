8c5093ad3f5be3fc6ded83b113c2ab5ff19349a2
Shubham Pawar <shubha.pawar2207@gmail.com>
2024-05-26 11:43:12 +0530

    adapter design pattern added

---
+++ designpatterns/structural/AdapterDP.java
@@ -0,0 +1,50 @@
+package designpatterns.structural;
+
+/**
+ * Used to work with existing system, which returns some result that needs to be
+ * refined so that client can use it.
+ * Basically adapting the existing functionality
+ */
+public class AdapterDP {
+    public static void main(String[] args) {
+        // if we directly get data from existing functionality
+        // we might receive data in unwanted format
+        WeatherReport weatherReport = new WeatherReport();
+        System.out.println(weatherReport.getTemperature() + " Kelvin");
+
+        // hence adding layer of adapter, which will convert the data in required format
+        TemperatureAdapter temperatureAdapter = new TemperatureAdapterImpl();
+        System.out.println(temperatureAdapter.requestTemperature() + " Celsius");
+    }
+}
+
+// WeatherReport functionality
+class WeatherReport {
+
+    // returns temperature in kelvin
+    public Double getTemperature(){
+        return 300.00;
+    }
+}
+
+// Adapter requests temperature report to existing functionality
+interface TemperatureAdapter {
+    Double requestTemperature();
+}
+
+// Request the temperature and performs the coversion
+// so that client can use it
+class TemperatureAdapterImpl implements  TemperatureAdapter {
+    private WeatherReport weatherReport;
+
+    public TemperatureAdapterImpl(){
+        weatherReport = new WeatherReport();
+    }
+
+    @Override
+    public Double requestTemperature() {
+        Double temperatureInKelvin = weatherReport.getTemperature();
+        Double temperatureInCelsius = temperatureInKelvin - 273.15;
+        return temperatureInCelsius;
+    }
+}
